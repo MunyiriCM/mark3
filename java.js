74e95c7e69159bc56d417d19a14b9760858658c8
MunyiriCM <mmunyiri12@gmail.com>
2024-09-05 11:40:16 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,26 @@
+let name1= 'Kiki';
+let age =30;
+
+console.log(name1)
+
+
+let name2='Mosh';
+let age3 =30;
+let isApproved= false;
+
+
+let person={
+
+    height:180,
+    jina: 'Mukuju',
+    location:'Ummo'
+};
+
+console.log(person)
+
+function greet(name,lastName){
+
+    console.log('Hello '+ name +' '+  lastName)
+}
+
+greet('Mark', 'Ciira');
